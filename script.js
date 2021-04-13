efe5cc8b05bfa83d9499797b0b7f82ef11b266dc
Sean <zimbiekitten@gmail.com>
2021-04-13 16:35:37 -0700

    stats done

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let header = document.getElementById('header');
+let section = document.getElementById('section');
+
 let file = location.pathname;
 file = file.split('/').join('').split('.html').join('.json');
 
@@ -6,5 +9,27 @@ fetch(file)
     .then(data => init(data));
 
 function init(json) {
-    
+
+    // Header
+
+
+
+    // Section
+    console.log(json['stats'][0]['stat']['name']);
+    for(stat of json['stats']){
+        const ARTICLE = document.createElement('article');
+        const H3 = document.createElement('h3');
+        const P1 = document.createElement('p');
+        const P2 = document.createElement('p');
+
+        H3.textContent = stat['stat']['name'];
+        P1.textContent = `Base stat: ${stat['base_stat']}`;
+        P2.textContent = `Effort: ${stat['effort']}`;
+
+        ARTICLE.appendChild(H3);
+        ARTICLE.appendChild(P1);
+        ARTICLE.appendChild(P2);
+
+        section.appendChild(ARTICLE);
+    }
 }
