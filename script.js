8b5ed9ab6813937d061e8a95f07254b3507e57db
Aliana Arroyo <Aarroy294@west-mec.local>
2021-04-13 18:20:54 -0700

    add

--- script.js
+++ script.js
@@ -8,5 +8,12 @@ fetch(file)
 function init(json) {
     const H1 = document.createElement("h1");
     H1.textContent=json["name"];
-    header.appendChild(H1)
+    header.appendChild(H1);
+
+    //create the subheader
+    const H1 = document.createElement("Id")
+    Id.textContent=json["base_experience"];
+    Id.appendChild(Id)
+
+
 }
